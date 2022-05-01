a0802a9c6b464b015484f2b7622974d192459f4e
Lin Y J <74194952+xup6YJ@users.noreply.github.com>
2022-05-01 11:19:40 +0800

    Update DataPreprocessing.py

--- Code/DataPreprocessing.py
+++ Code/DataPreprocessing.py
@@ -4,12 +4,11 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-
 #data
-features = pd.read_csv('D:/File_X/Help/hsu/sig_var.csv')
+features = pd.read_csv('internal.csv')  #convert path
 # features.head(5)
 
-# 探索資料
+# data summary
 # print('The shape of our features is:', features.shape)
 # features.describe()
 # features.head()
@@ -17,7 +16,7 @@ features = pd.read_csv('D:/File_X/Help/hsu/sig_var.csv')
 # checking null
 # print(features.isnull().sum())
 
-# 特徵轉換
+# featuring
 labels = np.array(features['ADL-group'])
 # Remove the labels from the features
 # axis 1 refers to the columns
@@ -50,6 +49,26 @@ pd.DataFrame(train_labels).to_csv(y_train_path, index = False)
 y_test_path = 'test_labels.csv'
 pd.DataFrame(test_labels).to_csv(y_test_path, index = False)
 
+# External validation data
+
+features = pd.read_csv('external.csv')  #convert path
+
+labels = np.array(features['ADL-group'])
+# Remove the labels from the features
+# axis 1 refers to the columns
+features= features.drop('ADL-group', axis = 1)
+features= features.drop('ID', axis = 1)
+# Saving feature names for later use
+feature_list = list(features.columns)
+# Convert to numpy array
+features = np.array(features)
+
+exv_x_path = 'ex_features.csv'
+pd.DataFrame(features).to_csv(exv_x_path, index = False)
+
+exv_y_path = 'ex_labels.csv'
+pd.DataFrame(labels).to_csv(exv_y_path, index = False)
+
 def read_features(x_train = x_train_path, y_train = y_train_path, x_test = x_test_path, y_test = y_test_path):
 
     train_features = pd.read_csv(x_train)
@@ -66,7 +85,15 @@ def read_features(x_train = x_train_path, y_train = y_train_path, x_test = x_tes
     
     return train_features, train_labels, test_features, test_labels
 
-
+def read_external_data(x = exv_x_path, y = exv_y_path):
+    
+    exv_features = pd.read_csv(x)
+    exv_features = exv_features.to_numpy()
+    
+    exv_labels = pd.read_csv(y)
+    exv_labels = exv_labels.to_numpy()
+    
+    return exv_features, exv_labels
     
     
 
