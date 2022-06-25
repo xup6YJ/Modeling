368e9f329d55272d80cd8069d57a7f8637265747
Lin Y J <74194952+xup6YJ@users.noreply.github.com>
2022-06-25 15:18:41 +0800

    Update Model_bootstrapping.py

--- Code/Model_bootstrapping.py
+++ Code/Model_bootstrapping.py
@@ -1,4 +1,7 @@
 
+# -*- coding: utf-8 -*-
+
+
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -14,7 +17,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 import scipy.stats as stats
+from sklearn.naive_bayes import GaussianNB
 
 import Evaluation as eva
 import Model as md
@@ -23,23 +28,22 @@ from DataPreprocessing2 import *
 # main
 
 # data
-data = dat('2018-2020.csv')
-# train_features, train_labels, test_features, test_labels = data.shuffle_dat()
+data = dat('internal.csv')  #internal data
+
+# main
+
+# data
+
 exv_x, exv_y = read_external_data()
 
-# fit the keras model on the dataset   
-# Rnn feature
-# rnn_train_features = np.array(train_features)
-# rnn_train_features = np.reshape(train_features, (train_features.shape[0], train_features.shape[1], 1))
-# rnn_train_features.shape
-
-def bootstrapping_result(iterations, model_type, x_exv = exv_x, y_exv = exv_y):
+#iteration = 1000
+def bootstrapping(model_type, x_exv = exv_x, y_exv = exv_y, iterations = 2):
     
     result_df = pd.DataFrame(columns=['TP', 'FP', 'FN', 'TN', 'Sensitivity', 'Specificity', 'PPV', 'NPV', 
                                       'Accuracy','F1Score', 'AUC'])
     exv_result_df = pd.DataFrame(columns=['TP', 'FP', 'FN', 'TN', 'Sensitivity', 'Specificity', 'PPV', 'NPV', 
                                   'Accuracy','F1Score', 'AUC'])
-
+    
     for i in range(iterations):
         
         print('No. ', i, ' iteration')
@@ -49,22 +53,20 @@ def bootstrapping_result(iterations, model_type, x_exv = exv_x, y_exv = exv_y):
         x_test = test_features
         y_test = test_labels
         
+        
         if model_type == 'DNN':
             train_shape = (train_features.shape[1],)
             model = md.dnn_model(train_shape)
             history  = model.fit(train_features, 
                                  train_labels, 
-                                 epochs = 10, batch_size = 16)  #paper 70 epoch  #can convert 10/20
+                                 epochs = 10, batch_size = 16)  #paper 70 epoch
             
         elif model_type == 'RNN':
-            rnn_train_features = np.array(train_features)
-            rnn_train_features = np.reshape(train_features, (train_features.shape[0], train_features.shape[1], 1))
-            
             train_shape = (rnn_train_features.shape[1], rnn_train_features.shape[2])
             model = md.rnn_model(train_shape)
             history  = model.fit(rnn_train_features, 
                                  train_labels, 
-                                 epochs = 10, batch_size = 16)  #paper 20 epoch  #can convert 10/20
+                                 epochs = 10, batch_size = 16)  #paper 20 epoch
             
             x_test = np.array(x_test)
             x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
@@ -72,7 +74,7 @@ def bootstrapping_result(iterations, model_type, x_exv = exv_x, y_exv = exv_y):
             x_exv = np.array(x_exv)
             x_exv = np.reshape(x_exv, (x_exv.shape[0], x_exv.shape[1], 1))
             
-        elif model_type == 'Random_Forest':
+        elif model_type == 'Random Forest':
             # Instantiate model with 1000 decision trees, 50 node size
             model = md.rf_model()
             history = model.fit(train_features, train_labels.ravel())
@@ -84,6 +86,14 @@ def bootstrapping_result(iterations, model_type, x_exv = exv_x, y_exv = exv_y):
         elif model_type == 'Logistic':
             model = md.logistic_reg()
             model.fit(train_features,train_labels.ravel())
+            
+        elif model_type == 'KNN':
+            model = KNeighborsClassifier(n_neighbors=5)
+            history = model.fit(train_features,train_labels.ravel())
+            
+        elif model_type == 'NBC':
+            model = GaussianNB()
+            history = model.fit(train_features,train_labels.ravel())
             
         # prediction
         prediction = model.predict(x_test)
@@ -115,131 +125,56 @@ def bootstrapping_result(iterations, model_type, x_exv = exv_x, y_exv = exv_y):
     return result_df, exv_result_df
 
 
-
-# Bootstrap Main
-iteration = 1000  #1000
-all_result_df = pd.DataFrame(columns=['TP', 'FP', 'FN', 'TN', 'Sensitivity', 'Specificity', 'PPV', 'NPV', 
-                                  'Accuracy','F1Score', 'AUC', 'Group'])
-
-all_eva_result_df = pd.DataFrame(columns=['TP', 'FP', 'FN', 'TN', 'Sensitivity', 'Specificity', 'PPV', 'NPV', 
-                                  'Accuracy','F1Score', 'AUC', 'Group'])
-
-
-#1: DNN, 2: RNN, 3: Logistic, 4: SVM, 5:RF
-
-#DNN Result
-boot_result, boot_eva_result = bootstrapping_result(iterations = iteration, model_type = 'DNN')
-path = 'boot_result/DNN_boot_result.csv'
-boot_result.to_csv(path, index = False)
-path = 'boot_result/DNN_boot_eva_result.csv'
-boot_eva_result.to_csv(path, index = False)
-
-boot_result['Group'] = 1
-boot_eva_result['Group'] = 1
-all_result_df = pd.concat([boot_result, all_result_df])
-all_eva_result_df = pd.concat([boot_result, all_eva_result_df])
-
-#RNN Result
-boot_result, boot_eva_result = bootstrapping_result(iterations = iteration, model_type = 'RNN')
-path = 'boot_result/RNN_boot_result.csv'
-boot_result.to_csv(path, index = False)
-path = 'boot_result/RNN_boot_eva_result.csv'
-boot_eva_result.to_csv(path, index = False)
-
-boot_result['Group'] = 2
-boot_eva_result['Group'] = 2
-all_result_df = pd.concat([boot_result, all_result_df])
-all_eva_result_df = pd.concat([boot_result, all_eva_result_df])
-
-
-#Logistic Result
-boot_result, boot_eva_result = bootstrapping_result(iterations = iteration, model_type = 'Logistic')
-path = 'boot_result/Logistic_boot_result.csv'
-boot_result.to_csv(path, index = False)
-path = 'boot_result/Logistic_boot_eva_result.csv'
-boot_eva_result.to_csv(path, index = False)
-
-boot_result['Group'] = 3
-boot_eva_result['Group'] = 3
-all_result_df = pd.concat([boot_result, all_result_df])
-all_eva_result_df = pd.concat([boot_result, all_eva_result_df])
-
-
-# #SVM Result
-# boot_result, boot_eva_result = bootstrapping_result(iterations = iteration, model_type = 'SVM')
-# path = 'boot_result/SVM_boot_result.csv'
-# boot_result.to_csv(path, index = False)
-# path = 'boot_result/SVM_boot_eva_result.csv'
-# boot_eva_result.to_csv(path, index = False)
-
-# boot_result['Group'] = 4
-# boot_eva_result['Group'] = 4
-# all_result_df = pd.concat([boot_result, all_result_df])
-# all_eva_result_df = pd.concat([boot_result, all_eva_result_df])
-
-
-# #Random_Forest Result
-# boot_result, boot_eva_result = bootstrapping_result(iterations = iteration, model_type = 'Random_Forest')
-# path = 'boot_result/Random_Forest_boot_result.csv'
-# boot_result.to_csv(path, index = False)
-# path = 'boot_result/Random_Forest_boot_eva_result.csv'
-# boot_eva_result.to_csv(path, index = False)
-
-# boot_result['Group'] = 5
-# boot_eva_result['Group'] = 5
-# all_result_df = pd.concat([boot_result, all_result_df])
-# all_eva_result_df = pd.concat([boot_result, all_eva_result_df])
-
-
-path = 'boot_result/all_result_df.csv'
-all_result_df.to_csv(path, index = False)
-path = 'boot_result/all_eva_result_df.csv'
-all_eva_result_df.to_csv(path, index = False)
-
-#Evaluation
-
-def anova_boot(all_result_df):
+def anova_boot(all_result_df, number_of_model):
     indexs = ['Sensitivity', 'Specificity', 'PPV', 'NPV', 'Accuracy','F1Score', 'AUC']
-    anova_matrix = x = np.zeros((4, 15))
+    anova_matrix = x = np.zeros((number_of_model+1, 15))
     
-    for i in range(4):
+    for i in range(number_of_model+1):
         
-        if i <3 :
-            anova_matrix[i, 0] = np.average(all_result_df['Sensitivity'][all_result_df['Group'] == i+1])
+        if i <number_of_model :
+            anova_matrix[i, 0] = np.mean(all_result_df['Sensitivity'][all_result_df['Group'] == i+1])
             anova_matrix[i, 1] = np.std(all_result_df['Sensitivity'][all_result_df['Group'] == i+1])
             
-            anova_matrix[i, 2] = np.average(all_result_df['Specificity'][all_result_df['Group'] == i+1])
+            anova_matrix[i, 2] = np.mean(all_result_df['Specificity'][all_result_df['Group'] == i+1])
             anova_matrix[i, 3] = np.std(all_result_df['Specificity'][all_result_df['Group'] == i+1])
             
-            anova_matrix[i, 4] = np.average(all_result_df['PPV'][all_result_df['Group'] == i+1])
+            anova_matrix[i, 4] = np.mean(all_result_df['PPV'][all_result_df['Group'] == i+1])
             anova_matrix[i, 5] = np.std(all_result_df['PPV'][all_result_df['Group'] == i+1])
             
-            anova_matrix[i, 6] = np.average(all_result_df['NPV'][all_result_df['Group'] == i+1])
+            anova_matrix[i, 6] = np.mean(all_result_df['NPV'][all_result_df['Group'] == i+1])
             anova_matrix[i, 7] = np.std(all_result_df['NPV'][all_result_df['Group'] == i+1])
             
-            anova_matrix[i, 8] = np.average(all_result_df['Accuracy'][all_result_df['Group'] == i+1])
+            anova_matrix[i, 8] = np.mean(all_result_df['Accuracy'][all_result_df['Group'] == i+1])
             anova_matrix[i, 9] = np.std(all_result_df['Accuracy'][all_result_df['Group'] == i+1])
             
-            anova_matrix[i, 10] = np.average(all_result_df['F1Score'][all_result_df['Group'] == i+1])
+            anova_matrix[i, 10] = np.mean(all_result_df['F1Score'][all_result_df['Group'] == i+1])
             anova_matrix[i, 11] = np.std(all_result_df['F1Score'][all_result_df['Group'] == i+1])
             
-            anova_matrix[i, 12] = np.average(all_result_df['AUC'][all_result_df['Group'] == i+1])
+            anova_matrix[i, 12] = np.mean(all_result_df['AUC'][all_result_df['Group'] == i+1])
             anova_matrix[i, 13] = np.std(all_result_df['AUC'][all_result_df['Group'] == i+1])
             
             anova_matrix[i, 14] = i+1
         
     anova_test = []
+           
     for index in indexs:
-        
-        a_result = stats.f_oneway(all_result_df[index][all_result_df['Group'] == 1],
-                       all_result_df[index][all_result_df['Group'] == 2],
-                       all_result_df[index][all_result_df['Group'] == 3])
-        
-        anova_test.append(a_result[1])
+         
+         #
+         x1 = pd.to_numeric(all_result_df[index][all_result_df['Group'] == 1])
+         x2 = pd.to_numeric(all_result_df[index][all_result_df['Group'] == 2])
+         x3 = pd.to_numeric(all_result_df[index][all_result_df['Group'] == 3])
+         x4 = pd.to_numeric(all_result_df[index][all_result_df['Group'] == 4])
+         x5 = pd.to_numeric(all_result_df[index][all_result_df['Group'] == 5])
+         #
+         a_result = stats.f_oneway(x1,x2,x3, x4, x5)
+         
+
+         a_result = round(a_result[1],5)
+         anova_test.append(a_result)
         
     for i, j in enumerate(range(0, 14, 2)):
         # print(i, j)
-        anova_matrix[3, j] = anova_test[i]
+        anova_matrix[number_of_model, j] = anova_test[i]
             
     anova_df = pd.DataFrame(anova_matrix, columns=['Sensitivity Mean', 'Sensitivity SD',
                                               'Specificity Mean', 'Specificity SD',
@@ -252,10 +187,57 @@ def anova_boot(all_result_df):
     
     return anova_df
 
-anova_df = anova_boot(all_result_df)
-anova_eva_df = anova_boot(all_eva_result_df)
+#Main
 
-path = 'boot_result/anova_df.csv'
-anova_df.to_csv(path, index = False)
-path = 'boot_result/anova_eva_df.csv'
-anova_eva_df.to_csv(path, index = False)
+#all of the model type reserved on this code
+all_model_type = ('DNN', 'RNN', 'Random Forest', 'SVM', 'Logistic', 'KNN', 'NBC')
+
+#choose the model you want here
+sel_model_type = ('Random Forest', 'SVM', 'Logistic', 'KNN', 'NBC')
+
+def boot_result(sel_model_type = sel_model_type):
+
+    all_result_df = pd.DataFrame(columns=['TP', 'FP', 'FN', 'TN', 'Sensitivity', 'Specificity', 'PPV', 'NPV', 
+                                      'Accuracy','F1Score', 'AUC', 'Group', 'Model_Type'])
+    
+    all_eva_result_df = pd.DataFrame(columns=['TP', 'FP', 'FN', 'TN', 'Sensitivity', 'Specificity', 'PPV', 'NPV', 
+                                      'Accuracy','F1Score', 'AUC', 'Group', 'Model_Type'])
+     
+    
+    if len(sel_model_type)>1:
+        
+        for i, mod in enumerate(sel_model_type):
+            if mod in all_model_type:
+                
+                #Result
+                boot_result, boot_eva_result = bootstrapping(model_type = mod)
+                # path = 'Random_Forest_boot_result.csv'
+                # boot_result.to_csv(path, index = False)
+                # path = 'Random_Forest_boot_eva_result.csv'
+                # boot_eva_result.to_csv(path, index = False)
+
+                boot_result['Group'] = i+1
+                boot_eva_result['Group'] = i+1
+                boot_result['Model_Type'] = mod
+                boot_eva_result['Model_Type'] = mod
+                
+                all_result_df = pd.concat([boot_result, all_result_df])
+                all_eva_result_df = pd.concat([boot_result, all_eva_result_df])
+        
+        
+        path = 'boot_result/all_result_df.csv'
+        all_result_df.to_csv(path, index = False)
+        path = 'boot_result/all_eva_result_df.csv'
+        all_eva_result_df.to_csv(path, index = False)   
+        
+        anova_df = anova_boot(all_result_df, len(sel_model_type))
+        anova_eva_df = anova_boot(all_eva_result_df, len(sel_model_type))
+
+        path = 'boot_result/anova_df.csv'
+        anova_df.to_csv(path, index = False)
+        path = 'boot_result/anova_eva_df.csv'
+        anova_eva_df.to_csv(path, index = False)
+
+
+boot_result() 
+
